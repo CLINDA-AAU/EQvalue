57b01ee789a40df53b901448e9ff627a985ba1e7
Lars Børty Nielsen <lars.borty@gmail.com>
2018-08-15 16:58:34 +0200

    Create EQpaste.R

---
+++ R/EQpaste.R
@@ -0,0 +1,13 @@
+#' The function creates a variable that is all the values together and can be used with the QALY function
+#'
+#' This only works if the data conforms to the standard naming of EQ-5D-5L variables according to the user 
+#' guide Which can be found at: https://euroqol.org/wp-content/uploads/2016/09/EQ-5D-5L_UserGuide_2015.pdf
+#' 
+
+
+EQpaste <- function(x) {
+
+x$STATE <- paste(x$MOBILITY, x$SELFCARE, x$ACTIVITY, x$PAIN, x$ANXIETY, sep="")
+
+  return(x)
+}
