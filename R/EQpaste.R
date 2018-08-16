3c4033ecf3b6294f7937840929cfc1fe274e57b8
Lars Børty Nielsen <lars.borty@gmail.com>
2018-08-16 09:50:20 +0200

    EQpaste function error

--- R/EQpaste.R
+++ R/EQpaste.R
@@ -3,9 +3,13 @@
 #' This only works if the data conforms to the standard naming of EQ-5D-5L variables according to the user 
 #' guide Which can be found at: https://euroqol.org/wp-content/uploads/2016/09/EQ-5D-5L_UserGuide_2015.pdf
 #' 
-#' The naming of the data should be: MOBILITY, SELFCARE, ACTIVITY, PAIN, ANXEITY
+#' The naming of the data should be: MOBILITY, SELFCARE, ACTIVITY, PAIN, ANXIETY
 #' 
-
+#' @param EQpaste Converts the columns into STATE which is the pasted values
+#' @return STATE which is used im the QALY function
+#' @export
+#' @examples
+#' EQpaste(df)
 
 EQpaste <- function(x) {
 
